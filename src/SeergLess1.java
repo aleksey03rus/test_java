b563ef916078a7fac60f5c08bcd059ceac8b1909
aleksey03rus <aleksey03rus@ngs.ru>
2017-03-09 21:09:06 +0700

    del import

--- src/SeergLess1.java
+++ src/SeergLess1.java
@@ -7,16 +7,15 @@ public class SeergLess1 {
     public static void main(String[] args) {
         int sum = 0;
         int count = 0;
-        int maxVes = 16;
-        int arrLength = 5;
-        int[] arr = {4, 2, 2, 9, 6};
+        int maxVes =Integer.parseInt(args[0]);
+        int[] arr = {4, 2, 5, 9, 6};
 
-        for (int i = 0; i < arrLength; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if ((arr[i] + sum) <= maxVes) {
                 sum += arr[i];
                 count++;
             }
         }
-        System.out.println("pogrujeno " + count + "  obshiy ves =" + sum);
+        System.out.println("pogrujeno " + count + "  obshiy ves= " + sum);
     }
 }
