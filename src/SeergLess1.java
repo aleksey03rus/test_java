4430dde400558c7da4fa1e600b22a63a89dbd8db
aleksey03rus <aleksey03rus@ngs.ru>
2017-03-09 16:54:59 +0700

    Initial commit

---
+++ src/SeergLess1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Alex on 09.03.2017.
+ */
+public class SeergLess1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        int maxVes = 16;
+        int arrLength = 5;
+        int[] arr = {4, 2, 2, 9, 6};
+
+        for (int i = 0; i < arrLength; i++) {
+            if ((arr[i] + sum) <= maxVes) {
+                sum += arr[i];
+                count++;
+            }
+        }
+        System.out.println("pogrujeno " + count + "  obshiy ves =" + sum);
+    }
+}
