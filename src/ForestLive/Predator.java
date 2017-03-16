a8d04c8165fe3176fa84dec61e27fa085a1e813c
aleksey03rus <aleksey03rus@ngs.ru>
2017-03-16 14:53:34 +0700

    убрал недописанный метод

--- src/ForestLive/Predator.java
+++ src/ForestLive/Predator.java
@@ -4,9 +4,4 @@ class Predator extends Animal {
     public boolean defense() {
         return Math.random() < 0.33 ? true : false;
     }
-
-       public boolean eat(Essens Eater, Essens food) {
-
-        return false;
-    }
 }
