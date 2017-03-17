edc0f70e8db289dc2e94f7726a5e8fb9969ae53b
aleksey03rus <aleksey03rus@ngs.ru>
2017-03-17 15:04:17 +0700

    добавил коструктор класса

--- src/ForestLive/Predator.java
+++ src/ForestLive/Predator.java
@@ -1,6 +1,11 @@
 package ForestLive;
 
 class Predator extends Animal {
+
+    Predator(int ves){
+        setVes(ves);
+        setKal(100);
+    }
     public boolean defense() {
         return Math.random() < 0.33 ? true : false;
     }
