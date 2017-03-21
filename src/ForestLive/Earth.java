9beb38df14c4e103d0096be78952b376aeca0e5f
aleksey03rus <aleksey03rus@ngs.ru>
2017-03-21 20:54:48 +0700

    - index )

--- src/ForestLive/Earth.java
+++ src/ForestLive/Earth.java
@@ -8,18 +8,18 @@ import java.util.Random;
 public class Earth {
     public static void main(String[] args) {
         ArrayList<Essens> arrayList = new ArrayList<>();
-        arrayList.add(0, new Predator("bear1", 300));
-        arrayList.add(1, new Predator("bear2", 351));
-        arrayList.add(2, new Predator("bear2", 352));
-        arrayList.add(3, new Predator("bear2", 353));
-        arrayList.add(4, new Herbivore("Elk"));
-        arrayList.add(5, new Garbage());
-        arrayList.add(6, new Garbage());
-        arrayList.add(7, new Garbage());
-        arrayList.add(8, new Garbage());
-        arrayList.add(9, new Garbage());
-        arrayList.add(10, new Garbage());
-        arrayList.add(11, new Plant("Elk"));
+        arrayList.add(new Predator("bear1", 300));
+        arrayList.add(new Predator("bear2", 351));
+        arrayList.add(new Predator("bear2", 352));
+        arrayList.add(new Predator("bear2", 353));
+        arrayList.add(new Herbivore("Elk"));
+        arrayList.add(new Garbage());
+        arrayList.add(new Garbage());
+        arrayList.add(new Garbage());
+        arrayList.add(new Garbage());
+        arrayList.add(new Garbage());
+        arrayList.add(new Garbage());
+        arrayList.add(new Plant("Elk"));
 
 
 
@@ -34,7 +34,7 @@ public class Earth {
                 if (arrayList.get(randElement) instanceof Predator || arrayList.get(randElement) instanceof Herbivore) {
                     if (arrayList.get(randElement).eat(arrayList.get(randElement2))) {
 
-                        System.out.println(arrayList.get(randElement).getClass()+" Eater   food " +arrayList.get(randElement2)+", sizeMass=" +arrayList.size());
+                        System.out.println(arrayList.get(randElement)+" Eater   food " +arrayList.get(randElement2)+", sizeMass=" +arrayList.size());
                         arrayList.remove(arrayList.get(randElement2));
                     }
                 }
